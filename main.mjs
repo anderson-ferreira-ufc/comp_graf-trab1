0c000662856b60a8d52e254505e7ef07eaf2a9d1
Anderson Ferreira <anderson97compeng@alu.ufc.br>
2025-06-20 01:25:14 -0300

    Adicionado Saturno

--- main.mjs
+++ main.mjs
@@ -10,6 +10,33 @@ camera.lookAt(0, 0, 0);
 const controls = new OrbitControls(camera, renderer.domElement);
 document.body.appendChild(renderer.domElement);
 
+let tempo_terra = 0;
+
+let orbita_merc = 0;
+let raio_merc = 2440;
+const merc_sol_dist = 28;
+
+let orbita_ven = 0;
+let raio_ven = 6052;
+const ven_sol_dist = 40;
+
+let orbita_terra = 0;
+let raio_terra = 6378;
+const terra_sol_dist = 60;
+
+let orbita_mar = 0;
+let raio_mar = 3396;
+const mar_sol_dist = 80;
+
+let orbita_jup = 0;
+let raio_jup = 71492;
+const jup_sol_dist = 100;
+
+let orbita_sat = 0;
+let raio_sat = 60268;
+const sat_sol_dist = 130;
+
+
 //Sol
 const geo_sol = new THREE.SphereGeometry(20, 32, 32);
 const material_sol = new THREE.MeshBasicMaterial({color:0xffe341});
@@ -30,7 +57,7 @@ function createGrid(size = 1300, divisions = 1000) {
 function mercurioTranslacao() {
     //Mercúrio
     const merc_loader = new THREE.TextureLoader().load('textures/merc_tex.jpg');
-    const geo_merc = new THREE.SphereGeometry(0.383, 32, 32);
+    const geo_merc = new THREE.SphereGeometry(raio_merc / raio_terra, 32, 32);
     const material_merc = new THREE.MeshBasicMaterial({map: merc_loader});
     const mercurio = new THREE.Mesh(geo_merc, material_merc);
     mercurio.position.set(0, 0, 0);
@@ -41,7 +68,7 @@ function mercurioTranslacao() {
 // Construção de Vênus.
 function venusTranslacao() {
     const ven_loader = new THREE.TextureLoader().load('textures/ven_tex.jpg');
-    const geo_ven = new THREE.SphereGeometry(0.949, 32, 32);
+    const geo_ven = new THREE.SphereGeometry(raio_ven / raio_terra, 32, 32);
     const material_ven = new THREE.MeshBasicMaterial({map: ven_loader});
     const venus = new THREE.Mesh(geo_ven, material_ven);
     venus.position.set(0, 0, 0);
@@ -51,7 +78,7 @@ function venusTranslacao() {
 
 function terraTranslacao() {
     const terra_loader = new THREE.TextureLoader().load('textures/terra_tex.jpg');
-    const geo_terra = new THREE.SphereGeometry(1, 32, 32);
+    const geo_terra = new THREE.SphereGeometry(raio_terra / raio_terra, 32, 32);
     const material_terra = new THREE.MeshBasicMaterial({map: terra_loader});
     const terra = new THREE.Mesh(geo_terra, material_terra);
     terra.position.set(0, 0, 0);
@@ -61,7 +88,7 @@ function terraTranslacao() {
 
 function marteTranslacao() {
     const mar_loader = new THREE.TextureLoader().load('textures/mar_tex.jpg');
-    const geo_mar = new THREE.SphereGeometry(0.532, 32, 32);
+    const geo_mar = new THREE.SphereGeometry(raio_mar / raio_terra, 32, 32);
     const material_mar = new THREE.MeshBasicMaterial({map: mar_loader});
     const marte = new THREE.Mesh(geo_mar, material_mar);
     marte.position.set(0, 0, 0);
@@ -71,7 +98,7 @@ function marteTranslacao() {
 
 function jupiterTranslacao() {
     const jup_loader = new THREE.TextureLoader().load('textures/jup_tex.jpg');
-    const geo_jup = new THREE.SphereGeometry(11.209, 32, 32);
+    const geo_jup = new THREE.SphereGeometry(raio_jup / raio_terra, 32, 32);
     const material_jup = new THREE.MeshBasicMaterial({map: jup_loader});
     const jupiter = new THREE.Mesh(geo_jup, material_jup);
     jupiter.position.set(0, 0, 0);
@@ -79,31 +106,24 @@ function jupiterTranslacao() {
     return jupiter;
 }
 
+function saturnoTranslacao() {
+    const sat_loader = new THREE.TextureLoader().load('textures/sat_tex.jpg');
+    const geo_sat = new THREE.SphereGeometry(raio_sat / raio_terra, 32, 32);
+    const material_sat = new THREE.MeshBasicMaterial({map: sat_loader});
+    const saturno = new THREE.Mesh(geo_sat, material_sat);
+    saturno.position.set(0, 0, 0);
+    scene.add(saturno);
+    return saturno;
+}
+
 const mercurio = mercurioTranslacao();
 const venus = venusTranslacao();
 const terra = terraTranslacao();
 const marte = marteTranslacao();
 const jupiter = jupiterTranslacao();
+const saturno = saturnoTranslacao();
 
-let tempo_terra = 0;
-
-let orbita_merc = 0;
-const merc_sol_dist = 28;
-
-let orbita_ven = 0;
-const ven_sol_dist = 40;
-
-let orbita_terra = 0;
-const terra_sol_dist = 60;
-
-let orbita_mar = 0;
-const mar_sol_dist = 80;
-
-let orbita_jup = 0;
-const jup_sol_dist = 100;
-
-
-const animationSpeed = 0.5; // Velocidade de animação.
+const animationSpeed = 1; // Velocidade de animação.
 
 scene.add(createGrid());
 
@@ -143,8 +163,14 @@ function animate() {
     orbita_jup = tempo_terra * (365 / 4331); // Ano Terrestre (dias) / Órbita de Júpiter (dias terrestre)
     jupiter.position.x = Math.sin(orbita_jup) * jup_sol_dist;
     jupiter.position.z = Math.cos(orbita_jup) * jup_sol_dist; 
-    jupiter.rotation.y = tempo_terra * (365 / 1); // Ano Terrestre (dias) / Rotação de Júpiter (dias terrestre)
+    jupiter.rotation.y = tempo_terra * (365 / 0.43); // Ano Terrestre (dias) / Rotação de Júpiter (dias terrestre)
     
+    // Parâmetros de transformação de Saturno.
+    orbita_sat = tempo_terra * (365 / 10747); // Ano Terrestre (dias) / Órbita de Júpiter (dias terrestre)
+    saturno.position.x = Math.sin(orbita_sat) * sat_sol_dist;
+    saturno.position.z = Math.cos(orbita_sat) * sat_sol_dist; 
+    saturno.rotation.y = tempo_terra * (365 / 1); // Ano Terrestre (dias) / Rotação de Júpiter (dias terrestre)
+
     controls.update();
 }
 animate();
